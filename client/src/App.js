3db0ec5475664ef5127bf138e38b087b252c470b
lcb42 <blatherwick.lucy@gmail.com>
2020-04-04 00:50:25 +0100

    Added react-bootstrap / created basic navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Enterprise from './Enterprise.js';
 import Kraken from './Kraken.js';
+
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
 
 import logo from './logo.svg';
 import './App.css';
@@ -11,19 +17,15 @@ export default function App() {
 	return (
 		<Router>
 			<div>
-				<nav>
-					<ul>
-						<li>
-							<Link to="/">Home</Link>
-						</li>
-						<li>
-							<Link to="/kraken">Kraken</Link>
-						</li>
-						<li>
-							<Link to="/enterprise">Enterprise</Link>
-						</li>
-					</ul>
-				</nav>
+				<Navbar bg="dark" variant="dark">
+					<Navbar.Brand href="/">Navbar</Navbar.Brand>
+					<Nav className="mr-auto">
+						<Nav.Link href="/"> Home</Nav.Link>
+						<Nav.Link href="/kraken">Kraken</Nav.Link>
+						<Nav.Link href="/enterprise">Enterprise</Nav.Link>
+					</Nav>
+					<Button variant="outline-info">Login</Button>
+				</Navbar>
 
 				<Switch>
 					<Route path="/enterprise">
