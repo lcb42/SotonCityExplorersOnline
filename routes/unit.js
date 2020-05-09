56b1824d302fcdced06224e44a5122c7236a2601
superdyoll <superdyoll@gmail.com>
2020-05-09 15:06:11 +0100

    Unit API and demo member

--- routes/unit.js
+++ routes/unit.js
@@ -6,6 +6,21 @@ const Models = require('../models/')
 authRouter.get('/', async (req, res, next) => {
     const units = await Models.Unit.findAll();
     res.send({units: units});
+});
+
+authRouter.get('/:id', async (req,res) => {
+    const unit = await  Models.Unit.findAll({where: {id: req.params.id}})
+    res.send(unit[0]);
+})
+
+authRouter.get('/:unitId/members', async (req,res) => {
+    const members = await  Models.Member.findAll({where: {unitId: req.params.unitId}})
+    res.send(members);
+})
+
+authRouter.get('/:unitId/members/:memberId', async (req,res) => {
+    const member = await  Models.Member.findAll({where: {unitId: req.params.unitId, id: req.params.memberId}})
+    res.send(member[0]);
 })
 
 module.exports = authRouter;
---
+++ seeders/20200509134827-demo-member.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Members', [{
+      unitId: 1,
+      username: 'Demo',
+      emoji: 'wrench',
+      leader: false,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }])
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Members', null, {});
+  }
+};
